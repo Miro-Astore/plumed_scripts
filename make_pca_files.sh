5a5c0ec1fd8b890ef4adbac7fb3bd580f2efc4eb
Miro-Astore <miro.astore@gmail.com>
2020-11-30 16:37:17 +1100

    working vector file construction (i hope, i thought it was fixed before but who knows)

--- make_pca_files.sh
+++ make_pca_files.sh
@@ -1,13 +1,15 @@
 #this will take a list of pca files with their last frame corresponding to the most extreme of the desired pca vector. it will then cat them together and make them readable to plumed. the occ and beta stuff is to fix plumed's hacky indexing rules. 
 #second argument $2 is the reference system 
 
-#usage bash plumed_scripts/make_pca_files.sh list_pcas.txt ref_average struct.pdb md_start_struct 
+#usage 
+# bash plumed_scripts/make_pca_files.sh list_pcas.txt ref_average_struct.pdb md_start_struct.pdb 
 
 count=1
 for i in $(cat $1); 
 do 
 
-	vmd -dispdev text $i -f  $3 -e  ./plumed_scripts/mark_reference_atoms.tcl 
+	echo "making vec files"
+	vmd -dispdev text $i -f $3 -e  ./plumed_scripts/mark_reference_atoms.tcl 
 echo "REMARK TYPE=OPTIMAL" > vec_struct.pdb
 cat marked.pdb | grep ^ATOM > file.pdb
 awk '{
@@ -24,7 +26,8 @@ if ( NF == 10) {
 ' file.pdb >> vec_struct.pdb
 echo "END" >> vec_struct.pdb
 
-	vmd -dispdev text $i -f  $3 -e  ./plumed_scripts/mark_reference_atoms.tcl 
+	echo "making ref files"
+	vmd -dispdev text $2 -f $3 -e  ./plumed_scripts/mark_reference_atoms.tcl 
 
 echo "REMARK TYPE=OPTIMAL" > reference_struct.pdb
 cat marked.pdb | grep ^ATOM > file.pdb
