e8a483169d7da140a187f12eb9114fc8f1debee6
Miro-Astore <miro.astore@gmail.com>
2020-12-05 22:19:07 +1100

    fixed 1d flooding and a bug in colvar checker

--- colvar_stdev.py
+++ colvar_stdev.py
@@ -2,14 +2,16 @@ import numpy as np
 
 count=0
 with open('COLVAR', 'r') as fhand:
-    file_lines = [line[:-1] for line in fhand if ((line.strip() != '') and (('#' in line) == False)) ] # remove the last character '\n'. **Remove empty lines**.
-mat_raw = [[float(term) for term in line.split()] for line in file_lines]
-mat = np.array(mat_raw)
-# then you can do whatever you like. eg: first column
-num_cols=np.shape(mat)[1]
-for i in range(num_cols-1):
-    print(np.std(mat[:,i+1]))
+    file_lines = [line[:-1] for line in fhand if ((line.strip() != '') and (('#' in line) == False))] # remove the last character '\n'. **Remove empty lines**.
+line_length=len(file_lines[0].split(' '))
 
+mat_raw = [[(float(term)) for term in (line.split())] for line in file_lines if len(line.split(' '))==line_length ]
+mat = np.array(mat_raw)
+#mat=np.array(mat[:-2])
+num_cols=np.shape(mat)[1]
+# then you can do whatever you like. eg: first column
+print(mat)
+for i in range(num_cols-1):
     print (' col ' + str (i+1) +  ' max ' + str(np.max(mat[:,i+1])) +  ' min , ' + str(np.min(mat[:,i+1])) +  ' std ' + str(np.std(mat[:,i+1])) )
 # reshape it to n by n matrix:
 #res = first_col.reshape((n, n))
